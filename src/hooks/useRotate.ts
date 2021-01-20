6451152358be9673a4bdd4a8347457fdcf3e5b84
Mitirev Ivan <mitirev.iv@reliab.tech>
2021-01-20 22:09:34 +0300

    added completeMove

--- src/hooks/useRotate.ts
+++ src/hooks/useRotate.ts
@@ -1,36 +1,44 @@
-import { useIsHotkeyPressed } from "react-hotkeys-hook";
-import { useThree } from "react-three-fiber";
 import { Handler } from "react-use-gesture/dist/types";
-import { Euler, Group, Vector3 } from "three";
+import { Euler, Group, MathUtils, Vector3 } from "three";
 
-const group = (groups: Group[], tempGroup: Group) => {
-  groups.forEach((gr) => {
-    tempGroup.attach(gr);
-  });
-};
-const rotate = (
-  tempGroup: Group,
-  axis: number,
-  direction: boolean,
-  rotateValue: number
-) => {
-  if (tempGroup.children.length !== 8 && tempGroup.children.length !== 9) {
-    console.warn("cannot rotate", tempGroup.children.length, "elements");
-    return;
-  }
-  tempGroup.rotateOnAxis(
-    new Vector3().setComponent(axis, 1),
-    direction ? rotateValue : -rotateValue
-  );
-};
-const dispose = (groups: Group[], tempGroup: Group, outerGroup: Group) => {
-  groups.forEach((gr) => {
-    outerGroup.attach(gr);
-    gr.position.round();
-  });
+class Rotator {
+  current = 0;
 
-  tempGroup.setRotationFromEuler(new Euler(0, 0, 0));
-};
+  group = (groups: Group[], tempGroup: Group) => {
+    groups.forEach((gr) => tempGroup.attach(gr));
+    this.current = 0;
+  };
+  rotate = (tempGroup: Group, axis: number, rotateValue: number) => {
+    if (tempGroup.children.length !== 8 && tempGroup.children.length !== 9) {
+      console.warn("cannot rotate", tempGroup.children.length, "elements");
+      return;
+    }
+    tempGroup.rotateOnAxis(new Vector3().setComponent(axis, 1), rotateValue);
+    this.current += rotateValue;
+  };
+
+  completeMove = (tempGroup: Group, axis: number) => {
+    console.log(MathUtils.radToDeg(this.current));
+
+    const rest = this.current % (Math.PI / 2);
+    if (rest > Math.PI / 4) {
+      this.rotate(tempGroup, axis, Math.PI / 2 - rest);
+    } else {
+      this.rotate(tempGroup, axis, -rest);
+    }
+    this.current = 0;
+  };
+  dispose = (groups: Group[], tempGroup: Group, outerGroup: Group) => {
+    groups.forEach((gr) => {
+      outerGroup.attach(gr);
+      gr.position.round();
+    });
+
+    tempGroup.setRotationFromEuler(new Euler(0, 0, 0));
+  };
+}
+
+const rotator = new Rotator();
 export const useRotate = ({
   tempGroup,
   outerGroup,
@@ -40,12 +48,17 @@ export const useRotate = ({
   outerGroup: Group;
   getFace: (cubieId: string, axis: number) => Group[];
 }): Handler<"drag", React.PointerEvent<Group>> => {
-  const isPressed = useIsHotkeyPressed();
-  return ({ event, movement, offset, down, direction, first, last, delta }) => {
-    // console.log({ movement, down, direction });
-    // const rotateValue = Math.PI / 2;
-    // const directionBool = !isPressed("shift");
-    // isPressed("space") && console.log("rotating");
+  return ({
+    event,
+    movement,
+    offset,
+    down,
+    direction,
+    first,
+    last,
+    delta,
+    cancel,
+  }) => {
     const axis = ((window as unknown) as any).axis;
 
     const groups = getFace(
@@ -54,12 +67,13 @@ export const useRotate = ({
     );
 
     if (first) {
-      group(groups, tempGroup);
+      if (rotator.current) cancel();
+      rotator.group(groups, tempGroup);
     } else if (last) {
-      dispose(groups, tempGroup, outerGroup);
+      rotator.completeMove(tempGroup, axis);
+      rotator.dispose(groups, tempGroup, outerGroup);
     } else {
-      const dir = Math.abs(direction[0] - direction[1]) <= 1;
-      rotate(tempGroup, axis, dir, delta[axis]);
+      rotator.rotate(tempGroup, axis, delta[axis]);
     }
   };
 };
